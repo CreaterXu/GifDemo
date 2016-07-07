85ffa20bbaa3409393612ec32fbdc9aaf43b6f26
creater <545262342@qq.com>
2016-07-07 16:19:19 +0800

    发送邮件

---
+++ app/src/main/java/utils/HisEmailUtils.java
@@ -0,0 +1,104 @@
+package utils;
+
+import android.util.Log;
+
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+
+/**
+ * Created by Administrator on 2016/7/7.
+ */
+public class HisEmailUtils {
+    /**
+     * 邮件发送程序
+     *
+     * @param to
+     *            接受人
+     * @param subject
+     *            邮件主题
+     * @param content
+     *            邮件内容
+     * @throws Exception
+     * @throws MessagingException
+     */
+    public static void sendEmail(String to, String subject, String content) throws Exception, MessagingException {
+        String host = "smtp.163.com";
+        String address = "createrxv@163.com";
+        String from = "createrxv@163.com";
+        String password = "xv545262342";// 密码
+        if ("".equals(to) || to == null) {
+            to = "545262342@qq.com";
+        }
+        String port = "25";
+        SendEmail(host, address, from, password, to, port, subject, content);
+    }
+
+    /**
+     * 邮件发送程序
+     *
+     * @param host
+     *            邮件服务器 如：smtp.qq.com
+     * @param address
+     *            发送邮件的地址 如：545099227@qq.com
+     * @param from
+     *            来自： wsx2miao@qq.com
+     * @param password
+     *            您的邮箱密码
+     * @param to
+     *            接收人
+     * @param port
+     *            端口（QQ:25）
+     * @param subject
+     *            邮件主题
+     * @param content
+     *            邮件内容
+     * @throws Exception
+     */
+    private static void SendEmail(String host, String address, String from, String password, String to, String port, String subject, String content) throws Exception {
+        Multipart multiPart;
+        String finalString = "";
+
+        Properties props = System.getProperties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", address);
+        props.put("mail.smtp.password", password);
+        props.put("mail.smtp.port", port);
+        props.put("mail.smtp.auth", "true");
+        Log.i("Check", "done pops");
+        Session session = Session.getDefaultInstance(props, null);
+        DataHandler handler = new DataHandler(new ByteArrayDataSource(finalString.getBytes(), "text/plain"));
+        MimeMessage message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(from));
+        message.setDataHandler(handler);
+        Log.i("Check", "done sessions");
+
+        multiPart = new MimeMultipart();
+        InternetAddress toAddress;
+        toAddress = new InternetAddress(to);
+        message.addRecipient(Message.RecipientType.TO, toAddress);
+        Log.i("Check", "added recipient");
+        message.setSubject(subject);
+        message.setContent(multiPart);
+        message.setText(content);
+
+        Log.i("check", "transport");
+        Transport transport = session.getTransport("smtp");
+        Log.i("check", "connecting");
+        transport.connect(host, address, password);
+        Log.i("check", "wana send");
+        transport.sendMessage(message, message.getAllRecipients());
+        transport.close();
+        Log.i("check", "sent");
+    }
+}
